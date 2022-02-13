84860903376cfaf686160cd2a19c3b97fc2b560b
Евгений Нифантьев <nifantyev@gmail.com>
2022-02-13 16:56:11 +0300

    Перенес useAppDispatch и useAppSelector в store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,5 @@
 import { createStore, combineReducers, Store } from 'redux';
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux';
 import serviceListReducer, {
   ServiceListState,
 } from '../reducers/serviceListReducer';
@@ -20,5 +21,9 @@ const store: Store<AppState> = createStore(rootReducer);
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch = () => useDispatch<AppDispatch>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default store;
